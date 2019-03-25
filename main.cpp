c897840b8285fb4f997cc75c500bb5c68fe854b2
Serhiy Rosovskyy <serhiyrosovskyy@Serhiys-MacBook-Air.local>
2019-03-25 18:26:00 +0200

    Multithreading added

--- main.cpp
+++ main.cpp
@@ -29,11 +29,9 @@ inline std::chrono::steady_clock::time_point get_current_time_fenced() {
 }
 
 template<class D>
-inline long long to_us(const D& d)
-{
+inline long long to_us(const D &d) {
     return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
 }
-
 
 struct configuration {
     double rel_err, abs_err;
@@ -44,9 +42,8 @@ struct configuration {
 };
 
 
-configuration read_conf(istream& cf)
-{
-    ios::fmtflags flags( cf.flags() ); // Save stream state
+configuration read_conf(istream &cf) {
+    ios::fmtflags flags(cf.flags()); // Save stream state
     cf.exceptions(std::ifstream::failbit); // Enable exception on fail
 
     configuration res;
@@ -69,26 +66,25 @@ configuration read_conf(istream& cf)
         getline(cf, temp);
         cf >> res.max_steps;
         getline(cf, temp);
-    }catch(std::ios_base::failure &fail) // Life without RAII is hard...
+    } catch (std::ios_base::failure &fail) // Life without RAII is hard...
     {
-        cf.flags( flags );
+        cf.flags(flags);
         throw; // re-throw exception
     }
-    cf.flags( flags );
-    if( res.x2 <= res.x1 ) {
+    cf.flags(flags);
+    if (res.x2 <= res.x1) {
         throw std::runtime_error("x1 should be <= x2");
     }
     if (res.y2 <= res.y1) {
         throw std::runtime_error("y1 should be <= y2");
     }
-    if( res.initial_steps < 10 ) {
+    if (res.initial_steps < 10) {
         throw std::runtime_error("Too few initial_steps");
     }
-    if( res.max_steps < 10 ) {
+    if (res.max_steps < 10) {
         throw std::runtime_error("Too small max_steps");
     }
-    if( res.abs_err <=0 || res.rel_err <= 0 )
-    {
+    if (res.abs_err <= 0 || res.rel_err <= 0) {
         throw std::runtime_error("Errors should be positive");
     }
 
@@ -96,35 +92,38 @@ configuration read_conf(istream& cf)
 }
 
 
-double func_to_integrate(const double& x, const double& y) {
+double func_to_integrate(const double &x, const double &y) {
     return sqrt(pow(x, 5)) + pow(y, 6);
 }
 
 
-template<typename func_T>
-double integrate(func_T func, double x1, const double& x2, double y1, const double& y2, size_t steps)
-{
-    double delta_x = (x2 - x1) / steps;
-    double delta_y = (y2 - y1) / steps;
+// template<typename func_T>
+double
+integrate(double (*func)(double const &, double const &), int start, int finish, configuration conf, size_t steps,
+          double &result) {
+    double delta_x = (conf.x2 - conf.x1) / steps;
+    double delta_y = (conf.y2 - conf.y1) / steps;
     double res = 0;
 
-    for (int i = 0; i < steps; i++) {
+    for (int i = start; i < finish; i++) {
         for (int j = 0; j < steps; j++) {
-            double area = func(x1 + (delta_x * i), y1 + (delta_y * j));
-            area += func(x1 + (delta_x * (i + 1)), y1 + (delta_y * j));
-            area += func(x1 + (delta_x * (i + 1)), y1 + (delta_y * (j + 1)));
-            area += func(x1 + (delta_x * i), y1 + (delta_y * (j + 1)));
+            double area = func(conf.x1 + (delta_x * i), conf.y1 + (delta_y * j));
+            area += func(conf.x1 + (delta_x * (i + 1)), conf.y1 + (delta_y * j));
+            area += func(conf.x1 + (delta_x * (i + 1)), conf.y1 + (delta_y * (j + 1)));
+            area += func(conf.x1 + (delta_x * i), conf.y1 + (delta_y * (j + 1)));
             area /= 4;
 
             res += delta_x * delta_y * area;
         }
     }
 
+    result += res;
+
     return res;
 }
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     string filename;
     if (argc != 3) {
@@ -134,68 +133,89 @@ int main(int argc, char* argv[]) {
     }
 
     filename = argv[2];
-    // int thread_num = std::atoi(argv[1]), result = 0;
-    // std::vector<std::thread> threads;
-    // threads.reserve(thread_num);
+    int thread_num = std::atoi(argv[1]);
+    std::vector <std::thread> threads;
 
     ifstream config_stream(filename);
-    if(!config_stream.is_open()) {
+    if (!config_stream.is_open()) {
         cerr << "Failed to open configuration file " << filename << endl;
         return 2;
     }
 
     configuration config;
-    try{
+    try {
         config = read_conf(config_stream);
-    }catch (std::exception& ex)
-    {
+    } catch (std::exception &ex) {
         cerr << "Error: " << ex.what() << endl;
         return 3;
     }
 
+    double result = 0;
+
     auto before = get_current_time_fenced();
 
-
     size_t steps = config.initial_steps;
-//    for (int i = 0; i < thread_num; ++i) {
-//        threads[i] = std::thread(integrate, func_to_integrate, steps / thread_num * i, steps / thread_num * (i + 1), config.x1, config.x2, config.y1, config.y2, steps);
-//        threads.emplace_back(integrate, func_to_integrate, steps / thread_num * i, steps / thread_num * (i + 1), config.x1, config.x2, config.y1, config.y2, steps);
-//    }
-//
-//    for (int i = 0; i < thread_num; ++i) {
-//        threads[i].join();
-//    }
-    double cur_res = integrate(func_to_integrate, config.x1, config.x2, config.y1, config.y2, steps);
+
+    for (int i = 0; i < thread_num; i++) {
+        threads.emplace_back(
+                std::thread(integrate, func_to_integrate, steps / thread_num * i, steps / thread_num * (i + 1), config,
+                            steps, std::ref(result)));
+    }
+
+    for (int i = 0; i < thread_num; ++i) {
+        threads[i].join();
+    }
+
+    threads.clear();
+
+    double cur_res = result;
+    result = 0;
     std::cout << cur_res << std::endl;
     double prev_res = cur_res;
     bool to_continue = true;
     double abs_err = -1; // Just guard value
     double rel_err = -1; // Just guard value
+
 // #define PRINT_INTERMEDIATE_STEPS
-    while( to_continue )
-    {
+    while (to_continue) {
 #ifdef PRINT_INTERMEDIATE_STEPS
         cout << cur_res << " : " << steps << " steps" << endl;
 #endif
         prev_res = cur_res;
         steps *= 2;
-        cur_res = integrate(func_to_integrate, config.x1, config.x2, config.y1, config.y2, steps);
-        abs_err = fabs(cur_res-prev_res);
-        rel_err = fabs( (cur_res-prev_res)/cur_res );
+
+        result = 0;
+
+        for (int i = 0; i < thread_num; i++) {
+            threads.emplace_back(
+                    std::thread(integrate, func_to_integrate, steps / thread_num * i, steps / thread_num * (i + 1), config,
+                                steps, std::ref(result)));
+        }
+
+        for (int i = 0; i < thread_num; ++i) {
+            threads[i].join();
+        }
+
+        threads.clear();
+
+        cur_res = result;
+        abs_err = fabs(cur_res - prev_res);
+        rel_err = fabs((cur_res - prev_res) / cur_res);
 #ifdef PRINT_INTERMEDIATE_STEPS
         cout << '\t' << "Abs err : rel err " << abs_err << " : " << rel_err << endl;
 #endif
 
-        to_continue = ( abs_err > config.abs_err );
-        to_continue = to_continue && ( rel_err > config.rel_err );
+        to_continue = (abs_err > config.abs_err);
+        to_continue = to_continue && (rel_err > config.rel_err);
         to_continue = to_continue && (steps < config.max_steps);
     }
 
     auto time_to_calculate = get_current_time_fenced() - before;
 
+    cout << "DOUBLE RESULT: " << result << endl;
     cout << "Result: " << cur_res << endl;
     cout << "Abs err : rel err " << abs_err << " : " << rel_err << endl;
-    cout << "Time: " << to_us(time_to_calculate)  << endl;
+    cout << "Time: " << to_us(time_to_calculate) << endl;
 
     return 0;
 }
